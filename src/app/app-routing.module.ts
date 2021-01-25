a457a40eb580cecc7e8f9871f812641450de7d4f
IqraIdrees098 <iqra.ngxoft@gmail.com>
2021-01-25 18:18:12 +0500

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogComponent } from './blog/blog.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { RegularPostComponent } from './regular-post/regular-post.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ // { path: '',   redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'blogpost', component: BlogPostComponent },
+  { path: 'regularpost', component: RegularPostComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { RegularPostComponent } from './regular-post/regular-post.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    BlogComponent,
+    CategoriesComponent,
+    ShopComponent,
+    ProductComponent,
+    CartComponent,
+    BlogPostComponent,
+    RegularPostComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
